4b76765a815a88e4de2741846d93905c5e0c27f1
chambo-e <chambon.emmanuel@gmail.com>
2017-10-04 18:32:35 +0200

    feat: add a linter

--- routes/books.js
+++ routes/books.js
@@ -1,47 +1,45 @@
 
-var books = {
+const books = {
 
-	getAll: function(request, response) {
+	getAll(request, response) {
 		return response.status(200).json({
 			status: 200,
-			message: "GET all books"
+			message: 'GET all books'
 		});
 	},
 
-	getOne: function(request, response) {
+	getOne(request, response) {
 		return response.status(200).json({
 			status: 200,
-			message: "GET one book with id: " + request.params.id
+			message: `GET one book with id: ${request.params.id}`
 		});
 	},
 
-	create: function(request, response) {
+	create(request, response) {
 		return response.status(201).json({
 			status: 201,
 			success: true,
-			message: "CREATE book",
+			message: 'CREATE book',
 			body: request.body
 		});
 	},
 
-	update: function(request, response) {
+	update(request, response) {
 		return response.status(200).json({
 			status: 200,
 			success: true,
-			message: "UPDATE book with id: " + request.params.id,
+			message: `UPDATE book with id: ${request.params.id}`,
 			body: request.body
 		});
 	},
 
-	delete: function(request, response) {
+	delete(request, response) {
 		return response.status(200).json({
 			status: 200,
 			success: true,
-			message: "DELETE book with id: " + request.params.id
+			message: `DELETE book with id: ${request.params.id}`
 		});
-
 	}
 };
-
 
 module.exports = books;
