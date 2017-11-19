1ca2e08383c0ce63945b0cace758f342f5f739dd
Tommy <thomasmilox@gmail.com>
2017-11-19 00:05:09 +0000

    implemented authentication using jsonwebtoken

--- middleware/token.js
+++ middleware/token.js
@@ -1,11 +1,21 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = (request, response, next) => {
 	const token = (request.body && request.body.access_token) || (request.query && request.query.access_token) || request.headers['x-access-token'] || request.headers.authorization;
 
 	if (token) {
 		try {
-			// Decode token
-			console.log(`route: [${request.url}] | token: ${token}`);
-			next();
+			jwt.verify(token, '1S3cR€T!', (err, decoded) => {
+				if (err) {
+					return response.status(401).json({
+						status: 401,
+						success: false,
+						message: 'Failed to authenticate the token'
+					});
+				}
+				request.decoded = decoded;
+				next();
+			});
 		} catch (err) {
 			return response.status(500).json({
 				status: 500,
