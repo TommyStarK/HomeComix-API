62ee16d5e985a996616d1167691145bd682c7c44
Tommy <thomasmilox@gmail.com>
2017-12-01 22:45:51 +0000

    update authentication checks

--- middleware/token.js
+++ middleware/token.js
@@ -1,4 +1,6 @@
+const mongo = require('mongodb')
 const jwt = require('jsonwebtoken')
+const database = require('../database.js')
 
 module.exports = (request, response, next) => {
   const token = (request.body && request.body.access_token) ||
@@ -6,22 +8,41 @@ module.exports = (request, response, next) => {
       request.headers['x-access-token'] || request.headers.authorization
 
   if (token) {
-    try {
-      jwt.verify(token, '1S3cR€T!', (err, decoded) => {
-        if (err) {
-          return response.status(401).json({
-            status: 401,
-            success: false,
-            message: 'Failed to authenticate the token'
+    const db = database.get()
+
+    jwt.verify(token, '1S3cR€T!', (err, decoded) => {
+      if (err) {
+        return response.status(401).json({
+          status: 401,
+          success: false,
+          message: 'Invalid token'
+        })
+      }
+
+      request.decoded = decoded
+      try {
+        db.collection('users')
+          .findOne({ _id: new mongo.ObjectID(request.params.uid) })
+          .then(doc => {
+            if (doc === null) {
+              return response.status(401).json(
+                {status: 401, success: false, message: 'Nonexistent account'})
+            }
+
+            if (doc._id.toString() !== decoded.userId ||
+              request.params.uid !== doc._id.toString() ||
+              request.params.uid !== decoded.userId) {
+              return response.status(400).json(
+                {status: 400, success: false, message: 'Bad request'})
+            }
+
+            next()
           })
-        }
-        request.decoded = decoded
-        next()
-      })
-    } catch (err) {
-      return response.status(500).json(
-          {status: 500, success: false, message: 'Internal server error', err})
-    }
+      } catch (err) {
+        return response.status(500).json(
+          { status: 500, success: true, message: 'Internal server error' })
+      }
+    })
   } else {
     return response.status(401).json(
         {status: 401, success: false, message: 'No token provided'})
